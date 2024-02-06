bb6e265e096020a90b5bd18d1053b3cff03bab0c
SaraHussein80 <93788105+SaraHussein80@users.noreply.github.com>
2024-02-06 16:33:58 +0200

    home_screen.dart

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -1,45 +1,123 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:login/nav_bar_screens/nav_categories.dart';
+import 'package:login/nav_bar_screens/nav_main.dart';
+import 'package:login/nav_bar_screens/nav_setting.dart';
+import 'package:login/screens/login_screen.dart';
 
 class HomeScreen extends StatefulWidget {
-  String email, pass;
-
-  HomeScreen(this.email, this.pass, {super.key});
+  final String disName;
+  final String disPass;
+  const HomeScreen({Key? key, required this.disName, required this.disPass})
+      : super(key: key);
 
   @override
   State<HomeScreen> createState() => _HomeScreenState();
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  int pageIndex = 0;
+  List<Widget> pages = [
+    MainScreen(),
+    CategoreScreen(),
+    SettingScreen(),
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      pageIndex = index;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        width: double.infinity,
+      endDrawer: Container(
+        height: MediaQuery.of(context).size.height,
+        width: MediaQuery.of(context).size.width / 2,
+        color: Colors.white,
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
           children: [
-            Text(
-              'Email : ${this.widget.email}',
-              style: GoogleFonts.robotoCondensed(
-                fontWeight: FontWeight.bold,
-                fontSize: 20,
+            SizedBox(
+              height: 100,
+            ),
+            InkWell(
+              onTap: () {
+                Navigator.of(context)
+                    .pushReplacement(MaterialPageRoute(builder: (context) {
+                  return LoginScreen();
+                }));
+              },
+              child: Column(
+                children: [
+                  Text(
+                    'Email : ${this.widget.disName}',
+                    style: GoogleFonts.robotoCondensed(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 20,
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 25,
+                  ),
+                  Text(
+                    'Password: ${this.widget.disPass}',
+                    style: GoogleFonts.robotoCondensed(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 20,
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 25,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Icon(Icons.exit_to_app),
+                      Text(
+                        "LogOut",
+                        style: GoogleFonts.robotoCondensed(
+                          fontSize: 18,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      )
+                    ],
+                  ),
+                ],
               ),
-            ),
-            const SizedBox(
-              height: 25,
-            ),
-            Text(
-              'Password: ${this.widget.pass}',
-              style: GoogleFonts.robotoCondensed(
-                fontWeight: FontWeight.bold,
-                fontSize: 20,
-              ),
-            ),
+            )
           ],
         ),
       ),
+      appBar: AppBar(
+        elevation: 0.0,
+        backgroundColor: Colors.white,
+        iconTheme: IconThemeData(color: Colors.blue),
+      ),
+      body: pages[pageIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: pageIndex,
+        onTap: onnavBarTap,
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: "Home",
+          ),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.category), label: "Bookmarks"),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.data_array), label: "Details"),
+        ],
+        backgroundColor: Colors.white,
+      ),
     );
   }
+
+  onnavBarTap(int index) {
+    pageIndex = index;
+    setState(() {});
+  }
 }
+
+
+
