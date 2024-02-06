7f07ea12b1c5477d58665635ce153fc28af32e9b
SaraHussein80 <93788105+SaraHussein80@users.noreply.github.com>
2024-02-06 16:33:14 +0200

    nav_setting.dart

--- lib/nav_bar_screens/nav_setting.dart
+++ lib/nav_bar_screens/nav_setting.dart
@@ -1,73 +1,58 @@
+import 'dart:convert';
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:http/http.dart';
 
 class SettingScreen extends StatefulWidget {
-  const SettingScreen({super.key});
+  const SettingScreen({Key? key}) : super(key: key);
 
   @override
   State<SettingScreen> createState() => _SettingScreenState();
 }
 
 class _SettingScreenState extends State<SettingScreen> {
+  List data = [];
+  bool loding = false;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Padding(
-        padding: const EdgeInsets.all(20),
-        child: Column(
-          children: [
-            const SizedBox(height: 40),
-            CircleAvatar(
-              radius: 70,
-              backgroundImage: AssetImage('images/user.jpg'),
+        body: ListView(children: [
+      Container(
+          height: 50,
+          alignment: Alignment.topCenter,
+          margin: EdgeInsets.all(20),
+          decoration: BoxDecoration(
+              color: Colors.blue, borderRadius: BorderRadius.circular(15)),
+          child: InkWell(
+            child: Text(
+              "Click to show data",
+              style: TextStyle(fontSize: 30, color: Colors.white),
+              textAlign: TextAlign.center,
             ),
-            const SizedBox(height: 20),
-            itemProfile('Name', '*******', CupertinoIcons.person),
-            const SizedBox(height: 10),
-            itemProfile('Email', '************@gmail.com', CupertinoIcons.mail),
-            const SizedBox(
-              height: 20,
-            ),
-            SizedBox(
-              width: double.infinity,
-              child: ElevatedButton(
-                  onPressed: () {},
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor: Colors.blue,
-                    padding: const EdgeInsets.all(15),
-                  ),
-                  child: const Text(
-                    'Edit Profile',
-                    style: TextStyle(
-                      color: Colors.white,
-                    ),
-                  )),
-            )
-          ],
+            onTap: () async {
+              loding = true;
+              setState(() {});
+              var respo = await get(
+                  Uri.parse("https://jsonplaceholder.typicode.com/posts"));
+              var respobody = jsonDecode(respo.body);
+              data.addAll(respobody);
+              loding = false;
+              setState(() {});
+            },
+          )),
+      if (loding)
+        Center(
+          child: CircularProgressIndicator(),
         ),
-      ),
-    );
-  }
-
-  itemProfile(String title, String subtitle, IconData iconData) {
-    return Container(
-      decoration: BoxDecoration(
-          color: Colors.white,
-          borderRadius: BorderRadius.circular(10),
-          boxShadow: [
-            BoxShadow(
-                offset: Offset(0, 5),
-                color: Colors.blue.withOpacity(.2),
-                spreadRadius: 2,
-                blurRadius: 10)
-          ]),
-      child: ListTile(
-        title: Text(title),
-        subtitle: Text(subtitle),
-        leading: Icon(iconData),
-        trailing: Icon(Icons.arrow_forward, color: Colors.grey.shade400),
-        tileColor: Colors.white,
-      ),
-    );
+      ...List.generate(
+          data.length,
+          (index) => Card(
+                child: ListTile(
+                  title: Text("${data[index]['title']}"),
+                  subtitle: Text("${data[index]['body']}"),
+                ),
+              ))
+    ]));
   }
 }
